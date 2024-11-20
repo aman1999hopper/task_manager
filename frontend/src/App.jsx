3cc041dfa6d40898f185f0f2345ae4eeb8df959d
aman bhardwaj <amanbhardwaj5931@gmail.com>
2024-11-20 14:33:10 +0530

    Added frontend and backend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl text-center font-extrabold'>Here is new app</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
