a927a4a11b90c49caecd4f3dc18db7cf99bfec38
aman bhardwaj <amanbhardwaj5931@gmail.com>
2024-12-05 22:14:19 +0530

    Added dashboardcontent

---
+++ frontend/src/components/DashboardContent/DashboardContent.jsx
@@ -0,0 +1,11 @@
+const DashboardData = () => {
+  return (
+    <>
+      <div>
+        <h1>Hi, Welcome Back</h1>
+      </div>
+    </>
+  );
+};
+
+export default DashboardData;
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,3 +1,5 @@
+
+import DashboardData from "../components/DashboardContent/DashboardContent";
 import Sidebar from "../components/Sidebar/Sidebar";
 
 
@@ -5,6 +7,14 @@ const DashboardPage = () => {
   return (
     <>
     <Sidebar/>
+    <div className="flex-1 ml-56 p-6 bg-gray-100 ">
+        {/* You can add a navbar or header here if necessary */}
+        <header>
+          <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-800">Dashboard</h1>
+          <p className="text-sm text-gray-500 dark:text-gray-400">Welcome to your dashboard.</p>
+          <DashboardData />
+        </header>
+      </div>
     </>
   );
 };
