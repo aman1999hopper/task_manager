970060081efa4bfd495d704232ae8377a7102328
Aman Bhardwaj <amanbhardwaj5931@gmail.com>
2025-05-15 16:42:55 +0530

    Added headers and content inside server file

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 connectDB();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+    cors({ 
+    origin: "http://localhost:5173", 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"], 
+    })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
